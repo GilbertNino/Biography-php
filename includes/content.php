679f6e6e4853f965b0b34176bc490048f753a083
GilbertNino <101396507+GilbertNino@users.noreply.github.com>
2022-06-10 06:43:25 +0800

    Add files via upload

---
+++ includes/content.php
@@ -0,0 +1,33 @@
+<header>		
+		<h1><?php echo $title Biography of Student;?></h1>
+	</header>
+
+	<div class="container">
+		<section id="main-bio">
+			<h2> <?php echo $name Gilbert Niño G. Quiñones;?></h2>
+
+			<img id="bio-image" src="img/2x2.jpg" alt="me">
+
+			<h3> <?php echo $position College Student / Future Computer Engineer;?></h3>
+			<p>
+            <?php echo $p Gilbert Niño is a student of CITE and is currently in his 2nd year of College. He is 21 years old and likes to do extraneous activities and read online mangas. He dislike going out of the house. In his spare time Gilbert is involved in helping with the household chores and taking care of his nieces and nephew. His goal is to graduate college and get a good job so that he can help his parents.;?>
+			</p>
+		</section>
+
+		<section id="contact-info">
+
+			<h2><?php echo $contact Contact Information;?></h2>
+			
+			<ul>
+				<li><strong>Email:</strong> <a href="nino05.quinones@gmail.com">nino05.quinones@gmail.com</a></li>
+				<li><strong>Github:</strong> <a href="https://github.com/GilbertNino">https://github.com/Gilbert</a></li>
+				<li><strong>Tiktok:</strong> <a href="https://www.youtube.com/watch?v=hAq443fhyDo">Coming Soon!</a></li>
+				
+                
+			</ul>
+
+		</section>
+	</div>
+
+</body>
+</html>
---
+++ includes/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+	$title = "Biography of Student";
+    $link_style = "css/style.css";
+    ?>
+
+
+<?php include 'includes/head.php';?>
